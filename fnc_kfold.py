9ff1059614243a2c2986db14587c5e8623dfd370
FatimahAreola <fatimahyinka@gmail.com>
2022-04-03 15:49:58 -0400

    Update fnc_kfold.py

--- fnc_kfold.py
+++ fnc_kfold.py
@@ -5,15 +5,18 @@ nltk.download('wordnet')
 nltk.download('omw-1.4')
 import pandas as pd
 
-
 from sklearn.ensemble import GradientBoostingClassifier
 from feature_engineering import refuting_features, polarity_features, hand_features, gen_or_load_feats
 from feature_engineering import word_overlap_features
 from utils.dataset import DataSet
 from utils.generate_test_splits import kfold_split, get_stances_for_folds
 from utils.score import report_score, LABELS, score_submission
+nltk.download('punkt')
+nltk.download('wordnet')
 
 from utils.system import parse_params, check_version
+
+
 
 
 def generate_features(stances,dataset,name):
